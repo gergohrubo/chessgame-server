57a517d7f701c6e8e51daadf8fcc01cbc44a5663
nadia2612 <YOUR@EMAIL.com>
2019-12-17 15:11:13 +0100

    Post player

--- player/model.js
+++ player/model.js
@@ -3,13 +3,8 @@ const sequelize = require('../db')
 const Game= require("../game/model")
 const User=require("../user/model")
 
-const Player= sequelize.define('player', {
-  
-color:{
-  type:Sequelize.STRING,
-  allowNull: false
-}
-})
+const Player= sequelize.define('player'
+)
 Game.belongsToMany(User, { through: Player })
 User.belongsToMany(Game, { through: Player })
 module.exports = Player
--- player/router.js
+++ player/router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const{ toData}=require("../auth/jwt")
 const Player = require("./model");
 const { Router } = express
 
@@ -7,19 +8,29 @@ function factory(stream){
   const router = new Router();
 
 router.get("/player", async (req, res, next) => {
-  
-  try {
-    const palyers = await Player.findAll();
 
-    res.send(palyers);
+  try {
+    const players = await Player.findAll();
+
+    res.send(players);
   } catch (error) {
     next(error)
   }
 });
 
 router.post("/player", async (req, res, next) =>{
+
+  // const auth = req.headers.authorization && req.headers.authorization.split(' ')
+  // if (auth && auth[0] === 'Bearer' && auth[1]) {
+  //   const data = toData(auth[1])
+  //   res.send({data})
   try {
-    const player = await Player.create(req.body);
+    console.log('THE JWT',req.body.jwt)
+    console.log('THE TYPE', typeof req.body.jwt)
+    const {userId} = toData(req.body.jwt)
+    console.log('THE USER ID', userId)
+    console.log('THE TYPE OF USER ID', typeof userId)
+    const player = await Player.create({userId, gameId:req.body.gameId});  //userId, gameId:req.body.gameId}
 
 const action={
   type:"NEW_PLAYER",
@@ -27,12 +38,18 @@ const action={
 }
     const string=JSON.stringify(action)
     stream.send(string)
-    res.send(palyer);
+    res.send(player);
   } catch (error) {
     next(error);
   }
-  
+// } 
+// else {
+//   res.status(401).send({
+//     message: 'Please supply some valid credentials'
+//   })
+// }
 })
+
 return router
 }
 
